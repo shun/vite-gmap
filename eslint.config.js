06816d2947b3bf3f510725541879a9677f1bb6c6
KUDO Shunsuke <skudo@skudo-mbp.local>
2024-08-24 16:23:25 +0900

    eslint prettier のセットアップ

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,45 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslintParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist', 'node_modules'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        google: 'readonly', // google をグローバル変数として追加
+      },
+      parser: tseslintParser,
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint,
+      'prettier': prettier,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off', // React 17+では不要
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
-)
+];
---
+++ src/types/global.d.ts
@@ -0,0 +1,8 @@
+declare namespace google {
+  export interface maps {
+    Map: any;
+    SymbolPath: {
+      CIRCLE: any;
+    };
+  }
+}
